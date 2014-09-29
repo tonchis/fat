7e5050f3b69fa09a95a65f3c1e91618efd36f4cf
tonchis <lucas.tolchinsky@gmail.com>
2014-09-29 17:37:11 -0300

    WIP including Fat into Hash.

--- ext/fat/fat.c
+++ ext/fat/fat.c
@@ -3,26 +3,67 @@
 VALUE Fat = Qnil;
 
 void Init_fat();
-static VALUE method_at(VALUE self, VALUE hash, VALUE chain);
+static VALUE singleton_method_at(int argc, VALUE *argv, VALUE self);
+static VALUE module_method_at(int argc, VALUE *argv, VALUE hash);
+static VALUE fat(VALUE hash, VALUE fields);
 
 void Init_fat(void) {
   Fat = rb_define_module("Fat");
-  rb_define_singleton_method(Fat, "at", method_at, 2);
+  rb_define_module_function(Fat, "at", singleton_method_at, -1);
+  rb_define_method(Fat, "at", module_method_at, -1);
 }
 
-static VALUE method_at(VALUE self, VALUE hash, VALUE chain) {
-  const char* dot = ".";
-  StringValue(chain);
-  VALUE fields = rb_str_split(chain, dot);
+static VALUE singleton_method_at(int argc, VALUE *argv, VALUE self) {
+  VALUE hash;
+  VALUE args;
 
+  rb_scan_args(argc, argv, "1*", &hash, &args);
+
+  VALUE fields;
+  if(RARRAY_LEN(args) == 1) {
+    VALUE chain = RARRAY_PTR(args)[0];
+
+    StringValue(chain);
+    const char* dot = ".";
+
+    fields = rb_str_split(chain, dot);
+  } else {
+    fields = args;
+  }
+
+  return fat(hash, fields);
+}
+
+static VALUE module_method_at(int argc, VALUE *argv, VALUE hash) {
+  VALUE args;
+  rb_scan_args(argc, argv, "*", &args);
+
+  VALUE fields;
+  if(RARRAY_LEN(args) == 1) {
+    VALUE chain = RARRAY_PTR(args)[0];
+
+    StringValue(chain);
+    const char* dot = ".";
+
+    fields = rb_str_split(chain, dot);
+  } else {
+    fields = args;
+  }
+
+  return fat(hash, args);
+}
+
+static VALUE fat(VALUE hash, VALUE fields) {
   VALUE value = hash;
-  for(int i = 0; i < RARRAY_LEN(fields); i++) {
+  for (int i = 0; i < RARRAY_LEN(fields); i++) {
     value = rb_hash_aref(value, RARRAY_PTR(fields)[i]);
 
-    if(value == Qnil) {
-      return Qnil;
+    if (!RTEST(value)) {
+      return value;
     }
   }
 
   return value;
 }
+
+
--- test/fat_test.rb
+++ test/fat_test.rb
@@ -1,19 +1,58 @@
 require "cutest"
 require_relative "../lib/fat"
 
-setup do
-  {
-    "foo" => {
-      "bar" => {
-        "baz" => :found
+scope do
+  setup do
+    {
+      foo: {
+        "bar" => {
+          baz: :found
+        }
       }
     }
-  }
+  end
+
+  test "honor key type" do |hash|
+    assert_equal nil, Fat.at(hash, "foo", :not, :found)
+
+    assert_equal :found, Fat.at(hash, :foo, "bar", :baz)
+  end
 end
 
-test "find values from a chain of keys" do |hash|
-  assert_equal nil, Fat.at(hash, "foo.not.found")
+scope do
+  setup do
+    {
+      "foo" => {
+        "bar" => {
+          "baz" => :found
+        }
+      }
+    }
+  end
 
-  assert_equal :found, Fat.at(hash, "foo.bar.baz")
+  test "namespaced string keys" do |hash|
+    assert_equal nil, Fat.at(hash, "foo.not.found")
+
+    assert_equal :found, Fat.at(hash, "foo.bar.baz")
+  end
+end
+
+scope do
+  setup do
+    Hash.include(Fat)
+
+    {
+      foo: {
+        "bar" => {
+          baz: :found
+        }
+      }
+    }
+  end
+
+  test "including the module" do |hash|
+    assert hash.respond_to?(:at)
+    assert_equal :found, hash.at(:foo, "bar", :baz)
+  end
 end
 
